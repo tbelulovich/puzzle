42964a668056007058318faf68fd00a1bf068163
Tom Belulovich <tbelulovich@gmail.com>
2013-08-23 01:57:44 -0400

    * Puzzle: meta-module

---
+++ Puzzle.hs
@@ -0,0 +1,15 @@
+module Puzzle 
+  (module Puzzle.Char
+  ,module Puzzle.Dict
+  ,module Puzzle.Math
+  ,module Puzzle.Morse
+  ,module Puzzle.TVTropes
+  ,module Puzzle.Web
+  ) where
+
+import Puzzle.Char
+import Puzzle.Dict
+import Puzzle.Math
+import Puzzle.Morse
+import Puzzle.TVTropes
+import Puzzle.Web
