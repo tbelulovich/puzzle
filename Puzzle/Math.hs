723c09aaee67dcc39d76bb023210b8174ee0e08d
Tom Belulovich <tbelulovich@gmail.com>
2013-10-19 11:52:28 -0400

    use arithmoi for math ops

--- Puzzle/Math.hs
+++ Puzzle/Math.hs
@@ -1,11 +1,9 @@
-module Puzzle.Math where
+module Puzzle.Math 
+       ( module Math.NumberTheory.Primes 
+       , fac 
+       ) where
 
-primes = 2:(filter isPrime [3,5..])
-isPrime x = isPrime' x (takeWhile (\y -> (fromIntegral y) <= sqrt (fromIntegral x)) primes)
-  where isPrime' x [] = True
-        isPrime' x (y:ys) = if (x `mod` y == 0) 
-                               then False
-                               else isPrime' x ys
+import Math.NumberTheory.Primes
                                     
 fac 0 = 1
 fac n = n * (fac (n-1))
