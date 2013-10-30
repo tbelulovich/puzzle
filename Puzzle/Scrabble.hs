58095945f8ab889f1d3e619e7cbdac46a9cf713f
thobel <tbelulovich@gmail.com>
2013-10-30 16:17:07 -0400

    added phonespell scorer

--- Puzzle/Scrabble.hs
+++ Puzzle/Scrabble.hs
@@ -21,6 +21,10 @@ scrabble = scorerFromTable [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1
 wwf :: Scorer
 wwf = scorerFromTable  [1,4,4,2,1,4,3,3,1,10,5,2,4,2,1,4,10,1,1,1,2,5,4,8,3,10]
 
+phone :: Scorer 
+phone = scorerFromTable phoneTable
+  where phoneTable = [2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9]
+
 -- Usage: runReader (wordScore "HeLlO") scrabble
 letterScores :: String -> Scrabble [Int]
 letterScores s = asks (flip map s)
