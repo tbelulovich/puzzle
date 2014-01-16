6ac076955b8e0b65acd9317cebafe1019cfcef03
Thomas Belulovich <tbelulovich@gmail.com>
2014-01-16 04:21:19 -0500

    Morse module:
    * Reduced function exposure to just the parsers.
    * Hid ReadP.

--- Puzzle/Morse.hs
+++ Puzzle/Morse.hs
@@ -1,7 +1,12 @@
-module Puzzle.Morse where
+module Puzzle.Morse
+       (morseLetter
+       ,runMorse
+       ,morseLetter
+       ,morseBlobDict
+       ,morseWord
+       ,morseDictWord
+       ) where
 
-
-import Control.Applicative hiding (many)
 import Control.Monad
 import Data.Char
 import Data.List
@@ -24,57 +29,57 @@ toMorse :: String -> String
 toMorse = intercalate " "
           . mapMaybe (flip lookup morseTable)
 
+type Morse a = ReadP a
+runMorse :: Morse a -> ReadS a
+runMorse = readP_to_S
+
 morseParsers :: [ReadP Char]
 morseParsers = map build morseTable where
   build (x,enc) =
     do string enc
        return x
        
-morseLetter :: ReadP Char
+morseLetter :: Morse Char
 morseLetter = choice morseParsers
 
 -- Perform p until p fails
-eager :: ReadP a -> ReadP [a]
+eager :: Morse a -> Morse [a]
 eager p = do x <- many p
              fails p
              return x
              
-fails :: ReadP a -> ReadP ()
+fails :: Morse a -> Morse ()
 fails p = do x <- (many1 p <++ return [])
              guard (null x)
              
+mem :: String -> Dict -> Bool
 mem x d =
   (x `elem` ["A","I","AM","ON","IN","OF","SO","IT"] || (length x >= 3))
   && S.member x d
              
+
+morseBlobDict :: Dict -> Morse String
 morseBlobDict d =
   do x <- many1 morseLetter
      guard $ mem x d
      return x
 
+morseWord :: Morse String
 morseWord = manyTill h eof where
   h = do x <- morseLetter
          eof +++ (char ' ' >> return ())
          skipSpaces
          return x
          
+morseDictWord :: Dict -> Morse String
 morseDictWord d =
   do x <- sepBy1 morseLetter (char ' ')
      guard (S.member x d)
      return x 
 
-testBlob s = 
-  do d <- stdDict
-     let p = manyTill (morseBlobDict d) eof
-     return $ readP_to_S p s
-
 readInt :: String -> [(Int,String)]
 readInt s = [(x,"")]
   where x = read s
 
+wrd :: Morse String
 wrd =  many1 $ satisfy isAlpha
-
-testo = do
-  x <- wrd
-  fails wrd
-  return x
