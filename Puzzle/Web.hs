002077da326c9db0eda7f68cecc389aac5d1ce83
Thomas Belulovich <tbelulovich@gmail.com>
2014-01-16 04:26:46 -0500

    Module Web:
    Renamed functions to more appropriate names.

--- Puzzle/Web.hs
+++ Puzzle/Web.hs
@@ -5,20 +5,23 @@ import Text.HTML.TagSoup
 import Data.Maybe
 import Text.HTML.TagSoup.Tree
 
-getText :: String -> IO String
-getText url = do (_,s) <- curlGetString url []
-                 return s
+getUrl :: String -> IO String
+getUrl url = do
+  (_,s) <- curlGetString url []
+  return s
                  
-getTags :: String -> IO [Tag String]
-getTags url = do t <- getText url
-                 return $ parseTags t
+tagsFromUrl :: String -> IO [Tag String]
+tagsFromUrl url = do
+  t <- getUrl url
+  return $ parseTags t
 
 stripTags :: String -> IO [String]
-stripTags url = do t <- getTags url
-                   return $ mapMaybe maybeTagText t
+stripTags url = do
+  t <- tagsFromUrl url
+  return $ mapMaybe maybeTagText t
                 
-getTagTree :: String -> IO [TagTree String]
-getTagTree = (fmap tagTree) . getTags
+treeFromUrl :: String -> IO [TagTree String]
+treeFromUrl = (fmap tagTree) . tagsFromUrl
 
 type TT = TagTree String 
 type TreeParser a = TT -> [(a,TT)]
